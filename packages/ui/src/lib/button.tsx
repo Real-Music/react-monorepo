81992e8b360485b11b3fb0a8cc99d4d98ab447a5
Fedjio Raymond <fedjioraymond@gmail.com>
2025-08-28 14:23:24 +0100

    add p tag to button

--- packages/ui/src/lib/button.tsx
+++ packages/ui/src/lib/button.tsx
@@ -1,4 +1,9 @@
 export type ButtonProps = { label: string };
 export function Button({ label }: ButtonProps) {
-  return <button>{label}</button>;
+  return (
+    <button>
+      <p>hello world</p>
+      {label}
+    </button>
+  );
 }
